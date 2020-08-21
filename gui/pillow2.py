0a1c80c08da8dfe7210cb202c6618409eda15e0c
purelove2u <59471889+purelove2u@users.noreply.github.com>
2020-08-21 11:12:50 +0900

    0821

--- gui/pillow2.py
+++ gui/pillow2.py
@@ -1,10 +1,46 @@
+from tkinter import *
+from tkinter.filedialog import *
 from PIL import Image, ImageTk
 
-im = Image.open("./resource/dog.jpg")
 
-# Thumbnail 이미지 생성
-size = (64, 64)
-im.thumbnail(size)
+window = Tk()
+window.geometry("400x400")
+window.title("명화감상")
 
-im.save("./resource/python-thumb.jpg")
+
+def file_open():
+    fileName = askopenfilename(
+        parent=window,
+        filetypes=(
+            ("gif 파일", "*.gif"),
+            ("jpg 파일", "*.jpg *.jpeg"),
+            ("모든 파일", "*.*"),
+        ),
+    )
+    photo = ImageTk.PhotoImage(file=fileName)
+    # 아래 코드 둘다 있어야 가능
+    image_label.configure(image=photo)
+    image_label.image = photo
+
+
+# 메뉴 생성
+mainMenu = Menu(window)
+# 메뉴 안에 들어갈 메뉴 생성
+fileMenu = Menu(mainMenu)
+
+
+# 메뉴 붙이기
+window.config(menu=mainMenu)
+mainMenu.add_cascade(label="파일", menu=fileMenu)
+
+fileMenu.add_command(label="열기", command=file_open)
+fileMenu.add_separator()
+fileMenu.add_command(label="종료", command=quit)
+
+# 이미지 보여줄 라벨
+image_label = Label(window, image=None)
+image_label.pack(expand=1, anchor=CENTER)
+
+
+window.mainloop()
 
--- gui/pillow3.py
+++ gui/pillow3.py
@@ -1,10 +1,26 @@
-from PIL import Image, ImageTk
+from PIL import Image, ImageFilter
 
-im = Image.open("./resource/dog.jpg")
+image = Image.open("./resource/dog.jpg")
 
 # Thumbnail 이미지 생성
 size = (64, 64)
-im.thumbnail(size)
+image.thumbnail(size)
 
-im.save("./resource/python-thumb.jpg")
+image.save("./resource/python-thumb.jpg")
+
+
+# 크기 조정
+image2 = Image.open("./resource/dog.jpg")
+image2 = image.resize((600, 600))
+image2.save("./resource/dog-resize.jpg")
+
+
+# 회전
+image3 = Image.open("./resource/dog.jpg")
+image3.rotate(90).save("./resource/dog-rotate.jpg")
+
+
+# blur
+image4 = Image.open("./resource/dog.jpg")
+image4.filter(ImageFilter.BLUR).save("./resource/dog-blur.jpg")
 
