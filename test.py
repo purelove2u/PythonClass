bb048428f49944f0f742466b3a49b9230a61e4fe
purelove2u <purelovetou@gmail.com>
2020-10-20 18:03:00 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
-print("Hello World")
+
+s = "HELLO WORLD"
+print(s)
+print(s.lower())
+print(s)
+s = s.lower()
+print(s)
+
+
+x = [10, 20, 30]
+x.extend([40, 50])
+print(x)
+
+x.extend([40])
+print(x)
